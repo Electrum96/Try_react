dde76d5b94121dcd816be932b3e045f888e5e4bc
For_tuna <kachalina.kate96@mail.ru>
2023-08-18 18:40:43 +0300

    initial changes

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './header.scss';
+
+const Header = function() {
+
+    return (
+        <>
+        <div className="header">
+            <div className="header-container">
+                <span>Hello, world</span>
+
+            </div>
+        </div>
+        </>
+    )
+}
+export default Header;
