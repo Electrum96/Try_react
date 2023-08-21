4f430ed2debd7871ae86895cade7d1e24670c9f9
For_tuna <kachalina.kate96@mail.ru>
2023-08-21 18:19:39 +0300

    add request to the server

--- src/page/PageTodo.jsx
+++ src/page/PageTodo.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./pageTodo.scss";
 import Container from "../components/container/Container";
 import TodoBox from "../components/todo-box/TodoBox";
 
 const PageTodo = function () {
-const initState = [{id:1, title:"Title1"}, {id:2, title:"Title2"}, {id:3, title:"Title3"},{id:4, title:"Title4"}]
-const [todos, setTodos] = useState(initState);
+// const initState = [{id:1, title:"Title1"}, {id:2, title:"Title2"}, {id:3, title:"Title3"},{id:4, title:"Title4"}]
+const [todos, setTodos] = useState([]);
+useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos').then((answer) => answer.json()).then((json) =>  setTodos(json))
+}, [])
 
 function onDelete(id) {
 setTodos(todos.filter((todo) => todo.id !== id))
