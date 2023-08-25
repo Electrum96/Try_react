9d3ee487ef5224dc901fdf77ce6c57e14c3ac6de
For_tuna <kachalina.kate96@mail.ru>
2023-08-25 11:56:10 +0300

    add input

--- src/components/todo-box/TodoBox.jsx
+++ src/components/todo-box/TodoBox.jsx
@@ -1,10 +1,12 @@
-import React from "react";
 import './todoBox.scss';
 import Todo from "../todo/Todo";
+import Input from "../../input/Input";
 
-const TodoBox = function({todos, onDelete}) {
+const TodoBox = function({todos, onDelete, addTodo}) {
+
 
     return <div className="todo-box">
+        <Input addTodo={addTodo}/>
         {todos.map((todo) => <Todo onDelete={onDelete} key={todo.id} id={todo.id} title={todo.title}/>)}
         </div>
 
--- src/page/PageTodo.jsx
+++ src/page/PageTodo.jsx
@@ -4,20 +4,27 @@ import Container from "../components/container/Container";
 import TodoBox from "../components/todo-box/TodoBox";
 
 const PageTodo = function () {
-// const initState = [{id:1, title:"Title1"}, {id:2, title:"Title2"}, {id:3, title:"Title3"},{id:4, title:"Title4"}]
-const [todos, setTodos] = useState([]);
-useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos').then((answer) => answer.json()).then((json) =>  setTodos(json))
-}, [])
+const initState = [{id:1, title:"Title1"}, {id:2, title:"Title2"}, {id:3, title:"Title3"},{id:4, title:"Title4"}]
+const [todos, setTodos] = useState(initState);
+
+// useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/todos').then((answer) => answer.json()).then((json) =>  setTodos(json))
+// }, [])
 
 function onDelete(id) {
 setTodos(todos.filter((todo) => todo.id !== id))
 }
 
+function addTodo(value){
+  const newArray =todos.slice();
+  newArray.push({id:newArray.length+1, title:value});
+  setTodos(newArray);
+  console.log(newArray);
+}
 
   return (
   <Container>
-    <TodoBox  todos={todos} onDelete={onDelete}/>
+    <TodoBox  todos={todos} onDelete={onDelete}  addTodo={addTodo} />
   </Container>)
 };
 export default PageTodo;
